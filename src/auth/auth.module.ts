345e44b2dd9ca548cccbc43487c45e8812693548
Stanislav Lisovskiy <stas-lisovskii@mail.ru>
2023-04-11 21:47:34 +0300

    Initial commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaService } from '../prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from '../config/jwt.config';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+	imports: [ConfigModule, JwtModule.registerAsync({
+		imports: [ConfigModule],
+		inject: [ConfigService],
+		useFactory: getJwtConfig
+	})],
+	controllers: [AuthController],
+	providers: [AuthService, PrismaService, JwtStrategy]
+})
+export class AuthModule {
+}
